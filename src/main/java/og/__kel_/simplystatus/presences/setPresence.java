72e9897033fe8d08792d5ac8554c4550f3efdb3d
Кел <pavvvlovvva13@gmail.com>
2022-09-01 22:12:19 +0300

    Delete src/main directory

--- src/main/java/og/__kel_/simplystatus/presences/setPresence.java
+++
@@ -1,8 +0,0 @@
-package og.__kel_.simplystatus.presences;
-
-import club.minnced.discord.rpc.DiscordRichPresence;
-
-public class setPresence {
-    public setPresence(DiscordRichPresence presence){
-    }
-}
