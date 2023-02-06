171423540d79cca51b96b7a0d9007ff9ab48740a
Кел <pavvvlovvva13@gmail.com>
2023-02-06 22:30:07 +0300

    Delete src/main directory

--- src/main/java/og/__kel_/simplystatus/api/entity/executeEntity.java
+++
@@ -1,9 +0,0 @@
-package og.__kel_.simplystatus.api.entity;
-
-import club.minnced.discord.rpc.DiscordRichPresence;
-
-public class executeEntity {
-    public DiscordRichPresence getPresences(){
-        return new DiscordRichPresence();
-    }
-}
