c49131f8fd251812b8fb7788ca72214bf8d71dee
simply-kel <me@kelcuprum.ru>
2024-02-22 18:37:11 +0300

    Update files

--- Fabric/src/main/java/ru/kelcuprum/simplystatus/SimplyStatusFabric.java
+++ Fabric/src/main/java/ru/kelcuprum/simplystatus/SimplyStatusFabric.java
@@ -7,7 +7,6 @@ import net.fabricmc.loader.api.FabricLoader;
 public class SimplyStatusFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // ModList.get().isLoaded(modId);
         SimplyStatus.replayMod = FabricLoader.getInstance().getModContainer("replaymod").isPresent();
         SimplyStatus.waterPlayer = FabricLoader.getInstance().getModContainer("waterplayer").isPresent();
         SimplyStatus.svc = FabricLoader.getInstance().getModContainer("voicechat").isPresent();
