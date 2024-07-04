f68a2403fc23dd0b40b427a6a81cf94e382c0294
Kel <me@kelcuprum.ru>
2024-07-04 19:30:14 +0300

    Updated files - AlinLib 2.0.0-alpha.2 - Support 1.20.1 (maybe)

--- Common/src/main/java/ru/kelcuprum/simplystatus/info/SimplyPlayer.java
+++ Common/src/main/java/ru/kelcuprum/simplystatus/info/SimplyPlayer.java
@@ -4,7 +4,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
-import ru.kelcuprum.alinlib.config.parser.info.Player;
+import ru.kelcuprum.alinlib.info.Player;
 import ru.kelcuprum.simplystatus.SimplyStatus;
 import ru.kelcuprum.simplystatus.mods.Voice;
 
@@ -18,7 +18,7 @@ public class SimplyPlayer {
         else return SimplyStatus.USER.getNickname();
     }
     public static String getURLAvatar(){
-        if(Player.getProfileType().equalsIgnoreCase("msa") || Player.getProfileType().equalsIgnoreCase("mojang")){
+        if(Player.isLicenseAccount()){
             switch (SimplyStatus.userConfig.getNumber("USE_API_RENDER", 0).intValue()){
                 case 1 -> {
                     return "https://api.kelcuprum.ru/skin/render/avatar?name="+MINECRAFT.getUser().getName()+"&api=0&sendfile=true";
