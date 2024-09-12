7d640cbe22ffd299fc86f85d90771b41ebdc2f00
Kel <me@kelcuprum.ru>
2024-09-12 09:59:50 +0300

    Fixed icons

--- Common/src/main/java/ru/kelcuprum/simplystatus/presence/LoadingGame.java
+++ Common/src/main/java/ru/kelcuprum/simplystatus/presence/LoadingGame.java
@@ -1,6 +1,7 @@
 package ru.kelcuprum.simplystatus.presence;
 
 import com.jagrosh.discordipc.entities.*;
+import ru.kelcuprum.alinlib.AlinLib;
 import ru.kelcuprum.simplystatus.SimplyStatus;
 import ru.kelcuprum.simplystatus.config.Assets;
 
@@ -13,8 +14,11 @@ public class LoadingGame {
         RichPresence.Builder presence = new RichPresence.Builder().setActivityType(ActivityType.Playing);
         presence.setDetails("Loading Resources...");
         presence.setLargeImage(Assets.getSelected().getIcon("logo"));
-        if(SimplyStatus.isPEnable < 0.03) {
-            presence.setLargeImage("https://wf.kelcu.ru/simplystatus/what/wm/en_yawn.png");
+        if(AlinLib.isAprilFool()){
+            presence.setLargeImage("https://wf.kelcu.ru/mods/waterplayer/icons/clownfish.png");
+            presence.setState("The cackling carousel, it spins and never stops");
+        }else if(SimplyStatus.isPEnable < 0.03) {
+            presence.setLargeImage("https://wf.kelcu.ru/mods/simplystatus/what/wm/en_yawn.png");
             presence.setState("The world machine hasn't started");
         }
         SimplyStatus.sendPresence(presence.build());
